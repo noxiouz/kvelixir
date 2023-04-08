1427955782ba9ba16f52fe110ac2ac266ce5efab
Anton Tiurin <noxiouz@yandex.ru>
2023-04-08 15:57:04 +0100

    inmemory KV

--- lib/kv.ex
+++ lib/kv.ex
@@ -1,18 +1,44 @@
 defmodule KV do
-  @moduledoc """
-  Documentation for `Kv`.
-  """
+  use GenServer
 
-  @doc """
-  Hello world.
+  @me __MODULE__
 
-  ## Examples
+  def start(default \\ []) do
+    GenServer.start(__MODULE__, default, name: @me)
+  end
 
-      iex> KV.hello()
-      :world
+  def stop do
+    GenServer.stop(@me)
+  end
 
-  """
-  def hello do
-    :world
+  def get(key) do
+    GenServer.call(@me, {:get, key})
+  end
+
+  def put(key, value) do
+    GenServer.cast(@me, {:put, key, value})
+  end
+
+  def delete(key) do
+    GenServer.cast(@me, {:delete, key})
+  end
+
+
+  # Implementation
+
+  def init(_args) do
+    {:ok, %{}}
+  end
+
+  def handle_call({:get, key}, _from, state) do
+    {:reply, Map.get(state, key), state}
+  end
+
+  def handle_cast({:put, key, value}, state) do
+    {:noreply, Map.put(state, key, value)}
+  end
+
+  def handle_cast({:delete, key}, state) do
+    {:noreply, Map.delete(state, key)}
   end
 end
